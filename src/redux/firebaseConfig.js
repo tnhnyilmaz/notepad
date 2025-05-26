42decbd37949d8384a1ed68f0ce96cc72193f012
Tunahan Yılmaz <102863419+tnhnyilmaz@users.noreply.github.com>
2025-05-26 16:20:22 +0300

    Update firebaseConfig.js

--- src/redux/firebaseConfig.js
+++ src/redux/firebaseConfig.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBcJnciBaupOk7Da5MOam2vF3TTNZWAgLw",
-    authDomain: "notepad-9361a.firebaseapp.com",
-    projectId: "notepad-9361a",
-    storageBucket: "notepad-9361a.firebasestorage.app",
-    messagingSenderId: "124345785116",
-    appId: "1:124345785116:web:53d2080b16d336d7b778d9",
-    measurementId: "G-TJ8ZRQ8V02"
+    apiKey: "YOUR-API-KEY",
+    authDomain: "---+",
+    projectId: "-----",
+    storageBucket: "-----",
+    messagingSenderId: "-----",
+    appId: "-----+",
+    measurementId: "-----"
 };
 
 // Initialize Firebase
